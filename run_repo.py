2034378a8d3414ad3c8e28efb63b3df9fad64655
Pathos <29135741+Pathos14489@users.noreply.github.com>
2026-05-16 19:00:55 -0700

    updated the run_repo script to automatically install requirements with force-reinstall enabled. This will slow down the installation, but it'll make sure it's completely reinstalled correct every time. It also tracks the last installed requirements hash so it only reinstalls requirements when necessary

--- run_repo.py
+++ run_repo.py
@@ -2,6 +2,7 @@ import sys
 import os
 import argparse
 import json
+import hashlib
 import time
 
 class Logger:
@@ -42,6 +43,7 @@ launcher_logging = Logger() # Create a logger object to be used throughout the p
 
 repo_configs = "repo_configs/"
 repo_configs_path = os.path.join(os.getcwd(), repo_configs)
+repo_config_path = None
 python_path = "python-3.10.11-embed/python.exe"
 python_path = os.path.join(os.getcwd(), python_path)
 
@@ -61,6 +63,7 @@ try:
                     json_obj["dir_suffix"] = ""
                 # print(json_obj["repo"], args.repo_path, json_obj["dir_suffix"], args.dir_suffix)
                 if json_obj["repo"] == args.repo_path and json_obj["dir_suffix"] == args.dir_suffix:
+                    repo_config_path = os.path.join(repo_configs_path, file)
                     repo_json = json_obj
                     break
         if repo_json is None:
@@ -68,8 +71,50 @@ try:
         repo_path = os.path.join(os.getcwd(), "repositories\\" + args.repo_path.replace("/", "_") + repo_json["dir_suffix"])
         sys.path.append(repo_path)
         launcher_logging.info(sys.path)
+
+        python_path = repo_json.get("python_binary", python_path)
+        if "/" in python_path or "\\" in python_path:
+            python_path = os.path.join(os.getcwd(), python_path)
+        if not os.path.exists(python_path):
+            launcher_logging.error("Python binary not found at " + python_path + ", make sure the path is correct and that the python binary exists at that location. If you're using the launcher, please make sure that you downloaded the embedded python package for this repository and that the path to the python binary is correct in the repository config file. If you're running this script manually, you can specify the path to the python binary in the repository config file as well, just add a field called python_binary with the path to the python binary as the value.")
+            input("Press enter to exit...")
+            raise Exception("Python binary not found at " + python_path)
         # Change working directory to the repository
         os.chdir(repo_path)
+
+        needs_install = False
+
+        requirements_hash = None
+
+        requirements_filename = "requirements.txt"
+        # Windows
+        if os.name == 'nt':
+            # Check for win_requirements.txt and install if it exists
+            if os.path.exists(os.path.join(repo_path, "win_requirements.txt")):
+                requirements_filename = "win_requirements.txt"
+                launcher_logging.info("Found win_requirements.txt, using that instead of requirements.txt")
+
+        if repo_json["install_requirements"]:
+            with open(os.path.join(repo_path, requirements_filename), "rb") as f:
+                requirements_hash = hashlib.md5(f.read()).hexdigest()
+            launcher_logging.info("Requirements hash: " + requirements_hash)
+            if "requirements_hash" in repo_json and repo_json["requirements_hash"].strip() != "":
+                if repo_json["requirements_hash"] == requirements_hash:
+                    launcher_logging.info("Requirements have not changed since last run, skipping installation")
+                    needs_install = False
+                else:
+                    launcher_logging.info("Requirements have changed since last run, installing requirements")
+                    needs_install = True
+            else:
+                launcher_logging.info("No requirements hash found, installing requirements")
+                needs_install = True
+
+        if needs_install and not repo_json["install_requirements"]:
+            launcher_logging.warning("New requirements hash found but install_requirements is set to false, not installing requirements, but you should consider setting install_requirements to true to avoid this warning in the future! Pantella might stop working eventually if you don't install the requirements, so keep that in mind!")
+
+
+                
+
         # Set vargs
         sys.argv = [
             repo_json['entry_point'],
@@ -78,7 +123,6 @@ try:
         import sys
         sys.dont_write_bytecode = True
 
-        python_path = repo_json.get("python_binary", python_path)
         if "-3.11.6-" in python_path:
             # Fix PyTorch DLL loading issue on Windows for Python 3.11 https://github.com/pytorch/pytorch/issues/166628
             # You need to add this at the beginning, before importing torch and other conflicting modules
@@ -103,9 +147,13 @@ try:
 
 
         # Start the repository by running whatever entry point is specified to be as a cmd line arg
-        if repo_json["install_requirements"]:
-            command_script = python_path + " -m pip install -r " + os.path.join(repo_path, "requirements.txt")
+        if needs_install and repo_json["install_requirements"]:
+            command_script = python_path + " -m pip install -r " + os.path.join(repo_path, requirements_filename) + " --force-reinstall"
+            launcher_logging.info(command_script)
             os.system(command_script)
+            repo_json["requirements_hash"] = requirements_hash
+            with open(repo_config_path, "w") as f:
+                json.dump(repo_json, f, indent=4)
         exec(open(os.path.join(repo_path, repo_json['entry_point'])).read())
         
         # command_script = python_path + " " + os.path.join(repo_path, repo_json["entry_point"] + " " + " ".join(repo_json["args"]))
